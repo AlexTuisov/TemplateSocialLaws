a87bee13cd11ceb593181e1d4e029595c5bb9967
Alex Tuisov <queldelan@gmail.com>
2020-12-07 17:45:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyperplan
+import ma_val
+import sys
+
+
+def parse_template(template):
+    pass
+
+
+def verify_robustness(parsed_template):
+    pass
+
+
+def main(arguments):
+    template = parse_template()
+    result = verify_robustness(template)
+
+
+if __name__ == '__main__':
+    arguments = sys.argv
+    main(arguments)
+
